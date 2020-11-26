1da71d37745fc76ee0ee87963b7d088c8082d9b4
Ankit Parashar <ankitparashar88@gmail.com>
2020-11-26 21:05:48 +0530

    Committing the largestPrimeFactor problem for ProjectEuler.

---
+++ ProjectEuler/largestPrimeFactor.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 26 20:11:22 2020
+
+@author: Ankit Parashar
+"""
+#! largestPrimeFactor.py
+import math
+
+def isPrime(n):
+    if n % 2 == 0:
+        return False
+    for i in range(3, math.floor((math.sqrt(n) + 1))):
+        if n % i == 0:
+            return False
+    return True
+
+def largestPrimeFactor(n):
+    largest = 2
+    for i in range(2, math.floor((math.sqrt(n) + 1))):
+        if n % i == 0:
+            if isPrime(i):
+                if i > largest:
+                    largest = i
+    return largest
+
+#print(largestPrimeFactor(13195))
+print(largestPrimeFactor(600851475143))
